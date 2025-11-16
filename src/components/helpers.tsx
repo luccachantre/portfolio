cdc6569a20bd4b9981e02f8fa35071317a957724
Lucca Chantre <luccachantre@gmail.com>
2025-11-15 20:41:01 -0500

    learned more about typescript stuff, trying to understand what I'm implementing from the jgcarillo github example, tried explaining the new lines of code in comments in my own words, its confusing to read but its proof I'm trying

--- src/components/helpers.tsx
+++ src/components/helpers.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 
 export const usePlayerControls = () => {
     //this is a javascript "object", just like a json object, it stores key-value pairs
-    const keys = { keyW: 'forward', keyS: 'backward', keyA: 'left', keyD: 'right', Space: 'jump'}
+    const keys = { KeyW: 'forward', KeyS: 'backward', KeyA: 'left', KeyD: 'right', Space: 'jump'}
     const moveFieldByKey = (key: keyof typeof keys) => keys[key]
     //this line above let's us do something like moveFieldByKey('keyW') and it will give us 'forward'
     //keyof and typeof are typescript keywords
@@ -18,6 +18,47 @@ export const usePlayerControls = () => {
     //so putting it all together, that code/syntax is creating a special type on the fly to tell typescript
     //"(key) can only be one of the names in the keys object"
 
-    
+    const [movement, setMovement] = useState({ forward: false, backward: false, left: false, right: false, jump: false})
+
+    useEffect(() => {
+        
+        
+        /*
+        const handleKeyDown = (e: KeyboardEvent) => setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: true })) 
+        */
+        //theres a lot going on here, not sure I fully understand it to be honest, let me try to explain everything
+        //so we have a function looking for a keyboard event (e is the event), and we update setMovement, the useState
+        //and btw useState is an object with multiple fields in this case, not just one variable
+        //instead of just saying setMovement() and only putting the updated key, we use another arrow function/lambda
+        //and this is a very particular syntax for updating useState using an arrow function
+        //in this syntax the parameter, in our case m, represents the previous state, 
+        //and in the function part we have to specify the new state
+        //but as you can see we dont quite put code, we put an object, which is doable! a function can return an object
+        //and to do this we have to use () and then {} inside of it, so like () => ({fields in here})
+        //and so we use ... which is called spread operator
+        //it copies all fields from the object
+        //so in our new object we're saying take all the fields from m, which represents the previous state before the update
+        //then we also add a field 
+        //(which already exists, so once the code sees that the field names match it simply updates that existing field)
+        //and this field we add is moveFieldByKey(e.code) and e.code just grabs the name of the keyboard event, so like "KeyW" 
+        //and when we pass that into moveFieldByKey, it returns the corresponding string like 'forward'
+        //and that corresponds with the field names of the useState object, perfect! now we simply update that field to be true
+        //so to summarize all of this, when a key is pressed, we set that field of the usestate object to be 'true'
+        //to signal we are moving in that direction
+
+        //but that line still causes an error, since e.code is "typed" (like its type) as string and could cause an error, 
+        //so we need to say e.code will only be one of our specified keys
+        //to get around this issue we will define an array of valid keys as strings, then say e.code's type is that object's keys or something
+        const allowedKeys = ["KeyW", "KeyS", "KeyA", "KeyD", "Space"] as const
+        const handleKeyDown = (e: KeyboardEvent) => {
+            if (!allowedKeys.includes(e.code as any)) return 
+            setMovement((m) => ({...m, [moveFieldByKey(e.code as keyof typeof keys)]: true}))
+        }
+
+        const handleKeyUp = (e: KeyboardEvent) => {
+            if (!allowedKeys.includes(e.code as any)) return
+            setMovement((m) => ({...m, [moveFieldByKey(e.code as keyof typeof keys)]: false}))
+        }
+    }, [])
 
 }
